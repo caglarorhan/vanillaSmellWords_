c9136e9a93b092a0c36114b91cd78c66e17b7fb9
caglarorhan <caglaror@gmail.com>
2022-04-21 13:54:38 -0500

    TDK word dictionary meaning support added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const vSW = {
     dictionary:()=>{return dictionary},
     askedWordIndex:null,
     extraCharsCodes:[305,231,360,350,351,246,252,287],
+    meaningsOfWords:Object.create(null),
     init: () => {
         vSW.gameBoard.create()
             .then(() => {
@@ -21,7 +22,6 @@ const vSW = {
     },
     getRandomWordIndexFromDictionary: ()=>{
         return Math.floor(Math.random()*(dictionary.length-1));
-
     },
     gameBoard: {
         rowCount: 6,
@@ -46,14 +46,19 @@ const vSW = {
                 delete i.dataset.correctLetterCorrectPlace;
             });
             document.querySelectorAll( `#${vSW.name}-keyboard button`).forEach(btn=>{btn.disabled=false;})
+            vSW.hideTheMeaning();
         },
         setBoard: () => {
             for (let row = 0; row < vSW.gameBoard.rowCount; row++) {
                 let rowDiv = document.createElement('div');
                 rowDiv.style.cssText = vSW.cssStoryBook["game-board-row"];
+                rowDiv.dataset.divType="rows";
+                rowDiv.dataset.rowNo=''+row;
                 for (let col = 0; col < vSW.gameBoard.colCount; col++) {
                     let colDiv = document.createElement('div');
                     colDiv.style.cssText = vSW.cssStoryBook["game-board-col"];
+                    colDiv.dataset.divType="cols";
+                    colDiv.dataset.colNo=''+col;
                     rowDiv.appendChild(colDiv);
                     let letterInput = document.createElement('input');
                     letterInput.style.cssText = vSW.cssStoryBook["letterInput"];
@@ -61,6 +66,21 @@ const vSW = {
                     letterInput.setAttribute('readonly', "readonly");
                     colDiv.appendChild(letterInput);
                 }
+                rowDiv.addEventListener('mouseover',(event)=>{
+                    let srcItem = event.target;
+                    if(srcItem.dataset.divType==="cols"){
+                        srcItem=srcItem.parentNode;
+                    }
+                    if(vSW.gameBoard.guessedWords[parseInt(srcItem.dataset.rowNo)]){
+                        let focusedGuessedWord = vSW.gameBoard.guessedWords[parseInt(srcItem.dataset.rowNo)].join('');
+                        if(focusedGuessedWord.length>0){
+                            vSW.showTheMeaning(focusedGuessedWord);
+                        }
+                    }
+                })
+                rowDiv.addEventListener('mouseout',()=>{
+                    vSW.hideTheMeaning();
+                })
                 document.getElementById(vSW.name).appendChild(rowDiv);
             }
             // data created at localStorage
@@ -76,7 +96,6 @@ const vSW = {
             }
         },
         addChar: (char) => {
-
             if (!vSW.gameBoard.guessedWords.length) {
                 let newWord = [];
                 newWord.push(char);
@@ -111,8 +130,8 @@ const vSW = {
             let lastEnteredWord = vSW.gameBoard.guessedWords[guessedWordsLength-1];
             let askedWord = vSW.dictionary()[vSW.askedWordIndex];
             let askedWordLettersArray = askedWord.split('');
-            console.log(askedWord);
-            console.log(JSON.stringify(vSW.gameBoard.guessedWords))
+            // console.log(askedWord);
+            // console.log(JSON.stringify(vSW.gameBoard.guessedWords))
 
             if(lastEnteredWord.length!==vSW.gameBoard.colCount){
                 vSW.warningMessages(`You need to enter ${vSW.gameBoard.colCount} chars to make a guess!`);
@@ -124,13 +143,14 @@ const vSW = {
                 askedWordLettersArray.forEach(letter=>{
                     letterCountMap[letter]=++letterCountMap[letter] || 1;
                 })
-                console.log(JSON.stringify(letterCountMap))
+                // console.log(JSON.stringify(letterCountMap))
 
                 //if word guessed correctly
                 if(lastEnteredWord.join('')===askedWord){
                     vSW.gameBoard.endGame({didWin:true, message:"Bravvo... You found the asked word!"});
                 }else{
                     if(vSW.dictionary().includes(lastEnteredWord.join(''))){
+                        vSW.getTheMeaning(lastEnteredWord.join(''));
                         let newWord = [];
                         vSW.gameBoard.guessedWords.push(newWord);
                     }else{
@@ -211,9 +231,12 @@ const vSW = {
                 infoBox.innerHTML+= `<div>${k}:${v}</div>`;
             })
             infoBox.innerHTML+=`<hr>
+<h5>Color Hints</h5>
 <div style="text-align: left;"><span style="${vSW.cssStoryBook.correctLetterCorrectPlace}; width:20px !important; height:20px !important; display:inline-block;"> </span> correct letter correct place</div>
 <div style="text-align: left;"><span style="${vSW.cssStoryBook.correctLetterWrongPlace}; width:20px !important; height:20px !important; display:inline-block;"> </span> correct letter wrong place</div>
 <div style="text-align: left;"><span style="${vSW.cssStoryBook.wrongLetter}; width:20px !important; height:20px !important; display:inline-block;"> </span> wrong letter</div>
+<hr>
+author: <a href="${vSW.author}" target="_blank" rel="noopener noreferrer">Caglar Orhan</a>
 `;
 
             document.body.append(infoBox);
@@ -269,41 +292,79 @@ const vSW = {
         },
         cssStoryBook: {
             "body":"text-align:center;",
-            "game-board": "width: 40%; height: 400px; background-color: #f5f5f5; margin:10px auto;",
+            "game-board": `width: 40%; height: 400px; background-color: #f5f5f5; margin:10px auto 150px;`,
             "char": "width: 100px; height: 100px; background-color: #f5f5f5; border: 1px solid #000;",
-            "keyboard": "width: 40%; height: 10%; background-color: #f5f5f5;margin: 100px auto; 10px",
-            "keyboard-button": "width: 50px; height: 50px; cursor: pointer; corner-radius: 9px; font-weight:bold;",
-            "keyboard-button-enter": "width: 50px; height: 50px; cursor: pointer; corner-radius: 9px; background-color:darkgreen; color:white;",
-            "keyboard-button-delete": "width: 50px; height: 50px; cursor: pointer; corner-radius: 9px; background-color:red; color:white;",
-            "game-board-row": "width: 100%; height: 20%; display: flex; flex-direction: row;",
-            "game-board-col": "width: 20%; height: 100%; background-color: #f5f5f5; border: 1px solid #000;",
-            "letterInput": "width:100%; height:100%; color:red; font-size:3vw; text-align:center; font-weight:bold; cursor:pointer;",
+            "keyboard": "width: 40%; height: 10%; background-color: #f5f5f5; margin: 10px auto; 10px",
+            "keyboard-button": "width: 50px; height: 50px; cursor: pointer; corner-radius: 9px; font-weight:bold; user-select:none; ",
+            "keyboard-button-enter": "width: 50px; height: 50px; cursor: pointer; corner-radius: 20px; background-color:darkgreen; color:white;user-select:none;",
+            "keyboard-button-delete": "width: 50px; height: 50px; cursor: pointer; corner-radius: 9px; background-color:red; color:white;user-select:none; ",
+            "game-board-row": "width: 100%; height: 20%; display: flex; flex-direction: row; border:0px",
+            "game-board-col": "width: 20%; height: 100%; background-color: #f5f5f5; border: 1px solid #000; border:0px",
+            "letterInput": "width:100%; height:100%; color:red; font-size:3vw; text-align:center; font-weight:bold; cursor:pointer; user-select:none; pointer-events:none",
             "correctLetterCorrectPlace":"color:white; background-color:green",
             "correctLetterWrongPlace":"color:white; background-color:orange",
             "wrongLetter":"color:white; background-color:grey",
-            "infoBox":"border:1px; width:200px; height:200px; position:absolute; top:0; right:0; display:block;"
+            "infoBox":"border:1px; width:200px; height:200px; position:absolute; top:0; right:0; display:block;",
+            "showTheMeaningDiv":"text-align:left; width:300px;  margin:0 auto; top:30px; border:1px solid grey; position: absolute; padding:4px; overflow-y:auto; background-color:white;"
         },
     warningMessages:(message)=>{
         alert(message);
         console.warn(message);
+    },
+    getTheMeaning:(word)=>{
+        vSW.meaningsOfWords[word]=[];
+        fetch('https://sozluk.gov.tr/gts?ara='+word)
+            .then(data=>data.json())
+            .then(jsonData=>{
+                jsonData[0].anlamlarListe.forEach(item=>{
+                    //console.log(item.anlam);
+                    vSW.meaningsOfWords[word].push(item.anlam.toString());
+                })
+                vSW.showTheMeaning(word)
+            })
+
+    },
+    showTheMeaning:(word)=>{
+       let theWordMeaningDiv;
+           if(document.getElementById(`${vSW.name}-meanings`)){
+               theWordMeaningDiv = document.getElementById(`${vSW.name}-meanings`);
+           }else{
+               theWordMeaningDiv= document.createElement('div')
+               theWordMeaningDiv.id = `${vSW.name}-meanings`;
+           }
+           theWordMeaningDiv.style.cssText=vSW.cssStoryBook.showTheMeaningDiv;
+       theWordMeaningDiv.innerHTML=`<h4>${word.toLocaleUpperCase('tr')}</h4><ol>`;
+
+       vSW.meaningsOfWords[word].forEach(anlam=>{
+           theWordMeaningDiv.innerHTML+=`<li>${anlam}</li>`;
+       });
+        theWordMeaningDiv.innerHTML+=`<hr>Source: <a href="https://sozluk.gov.tr" target="_blank" rel="noopener noreferrer">TDK Dictionary</a>`;
+        let boardDivItem = document.querySelector(`#${vSW.name}`);
+        let rect = boardDivItem.getBoundingClientRect();
+        theWordMeaningDiv.style.top = rect.top+"px";
+        theWordMeaningDiv.style.left = (rect.left-300-10)+"px";
+        theWordMeaningDiv.style.height = (rect.bottom-rect-top)+"px";
+        document.body.append(theWordMeaningDiv);
+    },
+    hideTheMeaning:()=>{
+        if(document.querySelector(`#${vSW.name}-meanings`)){
+            document.querySelector(`#${vSW.name}-meanings`).remove();
+        }
     }
     }
 
 
 
-window.addEventListener('load',vSW.init);
+window.addEventListener('resize', vSW.hideTheMeaning);
 window.addEventListener('load',()=>{
+    vSW.init();
     document.body.style.cssText=vSW.cssStoryBook.body;
     let versionTag = document.createElement('div');
     versionTag.innerHTML=`version: ${vSW.version}`;
     document.body.insertAdjacentElement('afterbegin',versionTag);
-    let authorTag = document.createElement('div');
-    authorTag.innerHTML=`author: <a href="${vSW.author}" target="_blank" rel="noopener noreferrer">Caglar Orhan</a>`;
-    document.body.insertAdjacentElement('beforeend',authorTag);
 });
 
 document.addEventListener("keydown", event => {
-    console.log(event.key)
     if(event.key==="Enter"){
         document.querySelector(`#${vSW.name}-enter-button`).click();
         return;
